c2a4c842edd31dc21fb4231c77d436663ab74249
Kiwifuit <mahkiwi123@gmail.com>
2024-12-31 11:53:38 +0800

    feat: made code more interesting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut counter: u32 = 0;
+    loop {
+        counter += 1;
+        println!("I have been alive for {}s", counter);
+        sleep(Duration::from_secs(1));
+    }
 }
